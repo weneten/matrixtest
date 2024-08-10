d34a0befb31bf5389dc287c9ebda7a80b9611cfa
weneten <34011054+weneten@users.noreply.github.com>
2024-08-10 16:41:45 +0200

    Create validate.js

---
+++ static/js/validate.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('uploadForm');
+    const fileInput = document.getElementById('fileInput');
+    const MAX_FILE_SIZE = 100 * 1024 * 1024; // 100 MB
+
+    form.addEventListener('submit', function (event) {
+        const file = fileInput.files[0];
+        if (file && file.size > MAX_FILE_SIZE) {
+            event.preventDefault(); // Verhindert das Absenden des Formulars
+            alert(`Die Datei ist zu groß. Bitte wähle eine Datei, die kleiner als ${MAX_FILE_SIZE / 1024 / 1024} MB ist.`);
+        }
+    });
+});
